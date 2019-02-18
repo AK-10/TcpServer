0306873f602c22c4f5eec691b9cd5b26dea3fbb2
Atsushi KONISHI <crossover.ak@outlook.jp>
2019-02-18 21:31:54 +0900

    fix

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func main() {
 	}
 	fmt.Println("listening ", port)
 
-	// connectionの受付
 	for {
+		// 接続待ち
 		conn, err := listen.Accept()
 		if err != nil {
 			log.Fatal("can not established connection")
@@ -26,7 +26,8 @@ func main() {
 		// メモリ割り当て byte型はuint8型のエイリアス
 		buf := make([]byte, 1024)
 
-		fmt.Printf("[Remote Address]\n%s\n", conn.RemoteAddr())
+		fmt.Printf("[Remote Address]\n%s\n", conn.RemoteAddr()) // クライアントのアドレスを出力 ex.) 172.17.0.1:54696
+		// buf にリクエストを代入する.
 		n, err := conn.Read(buf) // nはリクエストのバイト列のサイズらしい
 		if err != nil {
 			log.Fatal("can not read request")
